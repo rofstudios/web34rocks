f5b03987ac21117224449c16742a2c3f02a670ce
Your Name <you@example.com>
2021-07-08 13:28:35 -0400

    intial commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 server.get('/api', (req, res) => {
     // console.log(req) //has a lot of information
-    res.json({message: "this is the code, the chosen code"})
+    res.json({message: "hello darkness my old friend"})
 })
 server.listen(PORT, () => {
     console.log(`I am listening at ${PORT}`)
